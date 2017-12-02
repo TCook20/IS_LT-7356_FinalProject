d9efa5ff8886e393e32600527ee3175453c40737
Travis Cook <>
2017-12-01 21:21:38 -0600

    External JS addition.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@
 * @author Travis Cook (cooktw@missouri.edu)
 *
 */
+
+$(document).ready(function(){
+    // Requirement 4c jQuery Event
+    $('.button').hover(function() {
+        // Requirement 4e jQuery Animation
+        $('.button').animate({ fontSize: "150%"});
+    }, function() {
+        $('.button').animate({ fontSize: "100%"});
+    });
+});
